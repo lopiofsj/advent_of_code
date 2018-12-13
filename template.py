ea4631d509b932ab4d61603dbe7c1d4366342d69
Matthew Lopez <mlopez2@apple.com>
2018-12-13 13:46:57 -0800

    Template (boilerplate) file

---
+++ template.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""Advent of code."""
+#
+# https://adventofcode.com/2018
+#
+import fileinput
+import argparse
+
+
+def do_work(files):
+    """For the data in the file(s), do the work of solving the puzzle."""
+
+    for line in fileinput.input(files=files):
+        print(line)
+
+
+###############################################################################
+###############################################################################
+###############################################################################
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="advent of code")
+    parser.add_argument('--file', '-f',
+                        help="use file for data instead of STDIN")
+
+    args = parser.parse_args()
+
+    if args.file:
+        input_files = [args.file, ]
+    else:
+        input_files = ['-', ]
+
+    do_work(input_files)
